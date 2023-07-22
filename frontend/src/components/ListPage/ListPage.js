c0688313506ccc731464107c6cf77cf57fd848e3
raj8888 <jadhavrj8877@gmail.com>
2023-07-22 14:53:26 +0530

    updated the search functionality

--- frontend/src/components/ListPage/ListPage.js
+++ frontend/src/components/ListPage/ListPage.js
@@ -1,42 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useCompanyContext } from '../../context/CompanyContext';
 import CompanyCard from './CompanyCard';
 
 const ListPage = () => {
-  const { companies, setCompanies, currentPage, totalPages, handleNextPage, handlePreviousPage } = useCompanyContext();
+  const { companies, currentPage, totalPages, handleNextPage, handlePreviousPage } = useCompanyContext();
   const limitPerPage = 5;
 
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [totalSearchResults, setTotalSearchResults] = useState(0);
+  const [showSearchResults, setShowSearchResults] = useState(false);
 
-  useEffect(() => {
-    if (searchQuery) {
-      const filteredCompanies = companies.filter((company) =>
-        company.companyName.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-      setSearchResults(filteredCompanies);
-      setTotalSearchResults(filteredCompanies.length);
-    } else {
-      setSearchResults([]);
-      setTotalSearchResults(0);
-    }
-  }, [searchQuery, companies]);
+  const handleSearch = () => {
+    const filteredCompanies = companies.filter((company) =>
+      company.companyName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setSearchResults(filteredCompanies);
+    setTotalSearchResults(filteredCompanies.length);
+    setShowSearchResults(true);
+  };
 
-  useEffect(() => {
-    fetchCompanies();
-  }, [currentPage, searchQuery]);
-
-  const fetchCompanies = async () => {
-    try {
-      const response = await fetch(`http://localhost:4500/companies/?page=${currentPage}&limit=${limitPerPage}`);
-      const data = await response.json();
-      // Update the companies state using the setCompanies function from the context
-      setCompanies(data.companies);
-      setTotalSearchResults(data.total); 
-    } catch (error) {
-      console.error('Error fetching companies:', error);
-    }
+  const handleClearSearch = () => {
+    setSearchQuery('');
+    setSearchResults([]);
+    setTotalSearchResults(0);
+    setShowSearchResults(false);
   };
 
   return (
@@ -49,18 +37,19 @@ const ListPage = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
           placeholder="Search by company name"
         />
-        <button onClick={() => { handleNextPage(); setSearchQuery(''); }}>Search</button>
+        <button onClick={handleSearch}>Search</button>
+        {showSearchResults && <button onClick={handleClearSearch}>Clear Search</button>}
       </div>
-      {searchQuery && <p>{`Showing ${totalSearchResults} search results`}</p>}
-      {(searchQuery && totalSearchResults > 0 ? searchResults : companies).map((company) => (
+      {showSearchResults && <p>{`Showing ${totalSearchResults} search results`}</p>}
+      {(showSearchResults && totalSearchResults > 0 ? searchResults : companies).map((company) => (
         <CompanyCard key={company._id} company={company} />
       ))}
       <div>
         <button onClick={handlePreviousPage} disabled={currentPage === 1}>
           Previous
         </button>
-        <span>{`Page ${currentPage} of ${searchQuery ? Math.ceil(totalSearchResults / limitPerPage) : totalPages}`}</span>
-        <button onClick={handleNextPage} disabled={currentPage === (searchQuery ? Math.ceil(totalSearchResults / limitPerPage) : totalPages)}>
+        <span>{`Page ${currentPage} of ${showSearchResults ? Math.ceil(totalSearchResults / limitPerPage) : totalPages}`}</span>
+        <button onClick={handleNextPage} disabled={currentPage === (showSearchResults ? Math.ceil(totalSearchResults / limitPerPage) : totalPages)}>
           Next
         </button>
       </div>
